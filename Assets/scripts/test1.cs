5bdbb711323e91f8ad77838014a2e38d7adcba60
Ray <771147270@qq.com>
2025-02-15 22:19:23 +0800

    第二次提交

--- Assets/scripts/test1.cs
+++ Assets/scripts/test1.cs
@@ -7,7 +7,7 @@ public class test1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //测试版本控制，这是最初版本，第一版
+        //测试版本控制，这是第二版
     }
 
     // Update is called once per frame
