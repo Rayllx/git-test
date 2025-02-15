10e9722d199204b6acc813d5ad80cc73825fca3e
Ray <771147270@qq.com>
2025-02-15 22:58:35 +0800

    回滚到第二版再改为第三版

--- Assets/scripts/test1.cs
+++ Assets/scripts/test1.cs
@@ -7,7 +7,7 @@ public class test1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //测试版本控制，这是第二版
+        //测试版本控制，这是第3版
     }
 
     // Update is called once per frame
